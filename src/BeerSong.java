586606136484c038d3324026f68ae8e62cd480d0
Nick K <nkerns25@yahoo.com>
2017-01-22 21:47:29 -0500

    Implemented "99 Bottles of Beer" song
    (Also updated gitignore to ignore IntelliJ IDEA files)
    This concludes the functionality specified in the homework.

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -15,13 +15,31 @@ public class BeerSong {
      */
     public static void main(String[] args) 
     {
-        
+        ninetyNineBottlesOfBeer();
     }
     /**
      * The following function prints the song.
      */
-    public static void ninetyNineBottlesOfBeer() 
+    private static void ninetyNineBottlesOfBeer()
     {
-        
+        for (int i = 99; i > 0; i--) {
+            // Get the current bottle's number as a string.
+            String current = getBottleString(i);
+            // Get the next bottle's string.
+            String next = getBottleString(i - 1);
+            // Print the song.
+            System.out.println(current + " of beer on the wall, " + current + " of beer");
+            System.out.println("take one down, pass it around, " + next + " of beer on the wall.");
+        }
+    }
+
+    /**
+     * This function converts the number of beers into a string for the song.
+     * @param beerCount The number of beers.
+     * @return A string with regards to how many bottles of beer are left.
+     */
+    private static String getBottleString(int beerCount)
+    {
+        return beerCount == 0 ? "no more bottles" : String.format("%d bottle%s", beerCount, beerCount > 1 ? "s" : "");
     }
 }
