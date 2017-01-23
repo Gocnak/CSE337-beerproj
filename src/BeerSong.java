af6ba8b0d0a9dcb8d63fdd570aa2049c477ec3b6
Nick K <nkerns25@yahoo.com>
2017-01-22 21:23:53 -0500

    Added beersong and empty methods

---
+++ src/BeerSong.java
@@ -0,0 +1,27 @@
+/**
+ * CSE 337 - Homework 2 - "Configuration Management"
+ * 
+ * In this file you will find the one-hit-wonder "99 Bottles of Beer" song.
+ * This song is beautiful and therefore so is the code. You aren't going to find
+ * this in any 200-level class.
+ *
+ * @author Nick Kerns
+ */
+public class BeerSong {
+
+    /**
+     * Main method, point of entry when running the program.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+    /**
+     * The following function prints the song.
+     */
+    public static void ninetyNineBottlesOfBeer() 
+    {
+        
+    }
+}
